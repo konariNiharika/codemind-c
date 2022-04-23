88e9a5977613132d8856a39cdd5a871ec9801dd9
konariNiharika <98024678+konariNiharika@users.noreply.github.com>
2022-04-23 09:59:33 +0530

    #include<stdio.h> int factorcount(int n) {     int i,fc=0;     for(i=1;i<=n;i++)     {         if(n%i==0)         fc++;     }     return fc; } int main() {     int n,fc;     scanf("%d",&n);     fc=factorcount(n);     if(fc==2)     {         printf("prime");     }     else     {         printf("not a prime");     } }

---
+++ prime_number.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int factorcount(int n)
+{
+    int i,fc=0;
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        fc++;
+    }
+    return fc;
+}
+int main()
+{
+    int n,fc;
+    scanf("%d",&n);
+    fc=factorcount(n);
+    if(fc==2)
+    {
+        printf("prime");
+    }
+    else
+    {
+        printf("not a prime");
+    }
+}
