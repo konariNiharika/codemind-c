be4e15cbb7aa983662aae399407b440d60a93ba5
konariNiharika <98024678+konariNiharika@users.noreply.github.com>
2022-06-22 11:43:28 +0530

    #include<stdio.h> int main() {     int n,i,arr[100],t,d,rev=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         rev=0;         t=arr[i];         while(arr[i])         {             d=arr[i]%10;             arr[i]=arr[i]/10;             rev=rev*10+d;         }         if(rev==t)         {             printf("True ");         }         else         {             printf("False ");         }     } }

---
+++ Palindrome_Number.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,arr[100],t,d,rev=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        rev=0;
+        t=arr[i];
+        while(arr[i])
+        {
+            d=arr[i]%10;
+            arr[i]=arr[i]/10;
+            rev=rev*10+d;
+        }
+        if(rev==t)
+        {
+            printf("True
+");
+        }
+        else
+        {
+            printf("False
+");
+        }
+    }
+}
