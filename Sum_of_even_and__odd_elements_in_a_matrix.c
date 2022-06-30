553b6031e1f01610348a85ed961516b0eb8cf2fd
konariNiharika <98024678+konariNiharika@users.noreply.github.com>
2022-06-30 19:48:54 +0530

    #include<stdio.h> int main() {     int m,n;     scanf("%d %d",&m,&n);     int a[m][n],i,j,sum=0,sum1=0;     for(i=0; i<m; i++)     {         for(j=0; j<n; j++)         {             scanf("%d",&a[i][j]);         }     }     for(i=0; i<m; i++)     {         for(j=0; j<n; j++)         {             if(a[i][j]%2==0)             {                 sum=sum+a[i][j];             }             if(a[i][j]%2!=0)             {                 sum1=sum1+a[i][j];             }         }     }     printf("%d %d",sum,sum1); }

---
+++ Sum_of_even_and__odd_elements_in_a_matrix.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int m,n;
+    scanf("%d %d",&m,&n);
+    int a[m][n],i,j,sum=0,sum1=0;
+    for(i=0; i<m; i++)
+    {
+        for(j=0; j<n; j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0; i<m; i++)
+    {
+        for(j=0; j<n; j++)
+        {
+            if(a[i][j]%2==0)
+            {
+                sum=sum+a[i][j];
+            }
+            if(a[i][j]%2!=0)
+            {
+                sum1=sum1+a[i][j];
+            }
+        }
+    }
+    printf("%d %d",sum,sum1);
+}
