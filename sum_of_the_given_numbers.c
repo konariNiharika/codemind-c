0e3171f4fbbe728e29587d2c961fd71ee09355fe
konariNiharika <98024678+konariNiharika@users.noreply.github.com>
2022-06-30 21:05:42 +0530

    #include<stdio.h> int main() {     int n,a,b,h,i;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d%d",&a,&b);         h=a+b;         printf("%d",h);         printf(" ");     } }

--- sum_of_the_given_numbers.c
+++ sum_of_the_given_numbers.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,a,b,c;
+    int n,a,b,h,i;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
-        scanf("%d %d",&a,&b);
-        c=a+b;
-        printf("%d
-",c);
+        scanf("%d%d",&a,&b);
+        h=a+b;
+        printf("%d",h);
+        printf("
+");
     }
 }
