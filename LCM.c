fc16dd3d8ba25a6fc90962183e45302ca0e8b6fd
konariNiharika <98024678+konariNiharika@users.noreply.github.com>
2022-06-30 20:55:08 +0530

    #include <stdio.h> int main() {     int n1, n2, max;     scanf("%d %d", &n1, &n2);
        // maximum number between n1 and n2 is stored in max
        max = (n1 > n2) ? n1 : n2;
    
        while (1) {
            if (max % n1 == 0 && max % n2 == 0) {
                printf("%d", max);
                break;
            }
            ++max;
        }
        return 0;
    }

--- LCM.c
+++ LCM.c
@@ -1,16 +1,17 @@
-#include<stdio.h>
-int main()
-{
-    int a,b;
-    scanf("%d",&a);
-    scanf("%d",&b);
-    int lcm=(a>b)?a:b;
-    while(1)
-    {
-        if(lcm%a==0&&lcm%b==0)
-        break;
-        lcm++;
+#include <stdio.h>
+int main() {
+    int n1, n2, max;
+    scanf("%d %d", &n1, &n2);
+
+    // maximum number between n1 and n2 is stored in max
+    max = (n1 > n2) ? n1 : n2;
+
+    while (1) {
+        if (max % n1 == 0 && max % n2 == 0) {
+            printf("%d", max);
+            break;
+        }
+        ++max;
     }
-    printf("%d",lcm);
     return 0;
 }
