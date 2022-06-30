9f9158e0662fbf6c0c1ebaba371d24bb9d28a2f1
konariNiharika <98024678+konariNiharika@users.noreply.github.com>
2022-06-30 21:12:16 +0530

    #include<stdio.h> int main() {     int n,d,temp,rev=0,rev2=0,p,h;     scanf("%d",&n);     temp=n;     h=n*n;     while(n!=0)     {         d=n%10;         n=n/10;         rev=rev*10+d;     }     p=rev*rev;     while(p!=0)     {         d=p%10;         p=p/10;         rev2=rev2*10+d;     }     if(rev2==h)     {         printf("True");     }     else     {         printf("False");     } }

--- Adam_number.c
+++ Adam_number.c
@@ -1,23 +1,25 @@
+
 #include<stdio.h>
 int main()
 {
-    int n,sq,rev=0,qs,d,s,v=0;
+    int n,d,temp,rev=0,rev2=0,p,h;
     scanf("%d",&n);
-    sq=n*n;
+    temp=n;
+    h=n*n;
     while(n!=0)
     {
         d=n%10;
         n=n/10;
         rev=rev*10+d;
     }
-    qs=rev*rev;
-    while(qs!=0)
+    p=rev*rev;
+    while(p!=0)
     {
-        s=qs%10;
-        qs=qs/10;
-        v=v*10+s;
+        d=p%10;
+        p=p/10;
+        rev2=rev2*10+d;
     }
-    if(sq==v)
+    if(rev2==h)
     {
         printf("True");
     }
