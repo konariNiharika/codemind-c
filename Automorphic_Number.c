424cf4fbfb5c4ef606ae368321ea3c003600c994
konariNiharika <98024678+konariNiharika@users.noreply.github.com>
2022-06-30 21:01:00 +0530

    #include<stdio.h> #include<stdlib.h> #include<math.h> int main() {   int num;   scanf("%d", &num);
      int sqr, temp, last;
      int count = 0;
     
      sqr = num * num;
      temp = num;
     
      //Counting digits
      while (temp > 0)
      {
        count++;
        temp = temp / 10;
      }
     
      //calculation
      int den = floor(pow(10, count));
      last = sqr % den;
     
      //display
      if (last == num)
        printf("Automorphic Number");
      else
        printf("Not an Automorphic Number");
     
      return 0;
    }

--- Automorphic_Number.c
+++ Automorphic_Number.c
@@ -1,21 +1,33 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
 int main()
 {
-    int n,sq,temp,p=1;
-    scanf("%d",&n);
-    sq=n*n;
-    temp=n;
-    while(n!=0)
-    {
-        n=n/10;
-        p=p*10;
-    }
-    if(sq%p==temp)
-    {
-        printf("Automorphic Number");
-    }
-    else
-    {
-        printf("Not an Automorphic Number");
-    }
+  int num;
+  scanf("%d", &num);
+ 
+  int sqr, temp, last;
+  int count = 0;
+ 
+  sqr = num * num;
+  temp = num;
+ 
+  //Counting digits
+  while (temp > 0)
+  {
+    count++;
+    temp = temp / 10;
+  }
+ 
+  //calculation
+  int den = floor(pow(10, count));
+  last = sqr % den;
+ 
+  //display
+  if (last == num)
+    printf("Automorphic Number");
+  else
+    printf("Not an Automorphic Number");
+ 
+  return 0;
 }
